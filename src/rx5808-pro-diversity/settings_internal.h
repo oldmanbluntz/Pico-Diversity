0d2aa4331dd6fd30f51fed3cacd2e45aa5d638c4
Mike Bailey <michael.l.bailey@hotmail.com>
2026-03-16 11:19:31 -0400

    Add files via upload

--- src/rx5808-pro-diversity/settings_internal.h
+++ src/rx5808-pro-diversity/settings_internal.h
@@ -7,7 +7,7 @@
 // === EEPROM ==================================================================
 
 // This should be incremented after every EEPROM change.
-#define EEPROM_MAGIC 0x0000002A
+#define EEPROM_MAGIC 0x0000002B
 
 // === Receiver Modules =========================================================
 
